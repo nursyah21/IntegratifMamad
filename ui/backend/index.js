cfd8ac09ad111ca354278b28def51149870a67d1
nursyahjaya <nursyahjaya@gmail.com>
2023-08-04 13:34:46 +0700

    update

--- ui/backend/index.js
+++ ui/backend/index.js
@@ -7,9 +7,13 @@ app.use(cors());
 app.use(bodyParser.json())
 
 
+// const LOGINREGIS = 'http://localhost:6969'
+// const AKBAR = 'http://localhost:8081'
+// const YURIDAN = 'http://localhost:9001'
+
 const LOGINREGIS = 'http://localhost:6969'
-const AKBAR = 'http://localhost:8081'
-const YURIDAN = 'http://localhost:9001'
+const AKBAR = 'http://akbar.serveo.net'
+const YURIDAN = 'http://yuridan.serveo.net'
 
 app.post('/registration', async (req, res)=> {
     var data = await fetch(LOGINREGIS + req.path, {
