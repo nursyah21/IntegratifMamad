4ad30084dd515d75b52ff1e97f6eab07f11f4d9d
nursyahjaya <nursyahjaya@gmail.com>
2023-08-06 22:22:17 +0700

    change to backend

--- ui/backend/index.js
+++ ui/backend/index.js
@@ -8,12 +8,12 @@ app.use(bodyParser.json())
 
 
 // const LOGINREGIS = 'http://localhost:6969'
-const AKBAR = 'http://localhost:8081'
-const YURIDAN = 'http://localhost:9001'
+// const AKBAR = 'http://localhost:8081'
+// const YURIDAN = 'http://localhost:9001'
 
-const LOGINREGIS = 'http://localhost:6969'
-// const AKBAR = 'http://akbar.serveo.net'
-// const YURIDAN = 'http://yuridan.serveo.net'
+const LOGINREGIS = 'http://loginregis.serveo.net'
+const AKBAR = 'http://akbar.serveo.net'
+const YURIDAN = 'http://yuridan.serveo.net'
 
 app.post('/registration', async (req, res)=> {
     var data = await fetch(LOGINREGIS + req.path, {
